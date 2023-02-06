d1b7057dfa18400d6395eb06726b119058003c67
Desh Iyer <desh.iyer@gmail.com>
2023-02-06 14:34:44 +0530

    Re-build source files

--- source/FIR.cpp
+++ source/FIR.cpp
@@ -2,8 +2,8 @@
 
 // define the variable in the stack memory, including declaration. The value of this
 // FIR_FILTER_IMPULSE_RESPONSE array is based on a design (the value represent the behaviour of the filter)
-// static float FIR_FILTER_IMPULSE_RESPONSE[FIR_FILTER_LENGTH] = {0.4, 0.3, 0.2, 0.1, 0.05};
-static float FIR_FILTER_IMPULSE_RESPONSE[FIR_FILTER_LENGTH] = {0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1};
+// static float FIR_FILTER_IMPULSE_RESPONSE[FIR_FILTER_LENGTH] = {0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1};
+static float FIR_FILTER_IMPULSE_RESPONSE[FIR_FILTER_LENGTH] = {0.4, 0.3, 0.2, 0.1, 0.05};
 
 // function to initialise the circular buffer value
 void FIRFilter_init(FIRFilter *fir)
