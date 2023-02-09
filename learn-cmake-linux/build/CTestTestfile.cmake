3780f5cdbd38afb19219ad4ec11cbcaca6b7fdee
Desh Iyer <desh.iyer@gmail.com>
2023-02-09 17:33:10 +0530

    Removed learn-cmake-linux/ directory

--- learn-cmake-linux/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/volt/code/learn-cmake-linux
-# Build directory: /home/volt/code/learn-cmake-linux/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
