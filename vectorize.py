ed3e6eee8b525132e46add44b0d9249d0cd65b28
Desh Iyer <desh.iyer@gmail.com>
2023-02-09 13:40:18 +0530

    Added file script to vectorize the .wave file

---
+++ vectorize.py
@@ -0,0 +1,25 @@
+import wave
+import struct
+import sys
+
+def wavToFloat(wave_file):
+    w = wave.open(wave_file)
+    astr = w.readframes(w.getnframes())
+    
+    # convert binary chunks to short 
+    a = struct.unpack("%ih" % (w.getnframes() * w.getnchannels()), astr)
+    a = [float(val) / pow(2, 15) for val in a]
+    
+    return a
+
+def main():
+    signal = wavToFloat(r'data/wrong-place-sound.wav')
+    
+    print("# Frames Read:", str(len(signal)))
+    print(f"Range of values read: [{str(min(signal))}, {str(max(signal))}]")
+    
+    print(type(signal))
+    
+    
+if __name__ == '__main__':
+    main()
