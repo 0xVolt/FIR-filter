787deefb62cb764f72ac1b73d387f809a5d43278
Desh Iyer <desh.iyer@gmail.com>
2023-02-10 16:33:47 +0530

    Updated dependencies

--- main.cpp
+++ main.cpp
@@ -5,32 +5,32 @@
 using namespace std;
 
 vector<float> loadSignal();
+void display(vector<float>);
 int getSignalSize(vector<float>);
+bool generateCommandsScript(vector<float>);
 
 int main(int argv, char *argc[]) {
     vector<float> signal = loadSignal();
 
-    // cout << getSignalSize(signal);
+    generateCommandsScript(signal);
 
     return 0;
 }
 
-int getSignalSize(vector<float> signal) {
-    int length = 0;
-
-    for (float element: signal)
-        length++;
-    
-    return length;
-} 
+void display(vector<float> signal) {
+    cout << "Displaying signal in float values:\n";
+    for (auto element: signal) {
+        cout << element << endl;
+    }
+}
 
 vector<float> loadSignal() {
     vector<float> signal;
 
     fstream file;
-    file.open("/home/volt/code/filter-coffee/data/signal.txt", ios_base::in);
+    file.open("/home/volt/code/filter-coffee/data/generated/signal.txt", ios_base::in);
 
-    for(string line; getline(file, line);) {
+    for (string line; getline(file, line);) {
         float value;
         istringstream readInput(line);
 
@@ -40,4 +40,75 @@ vector<float> loadSignal() {
     }
 
     return signal;
-} 
+}
+
+void generateCommandsScript(vector<float> signal) {
+    // fstream file;
+    // file.open("commands.p", ios_base::out);
+
+    // for (int i = 0; i < signal.size(); i++) {
+    //     file << signal[i] << endl;
+    // }
+
+    // file.close();
+
+    // return true;
+
+    int length = signal.size();
+
+    vector<float> filteredOutput(length);
+
+    // FIR filter with circular buffer
+    // Declaring the filter struct variable
+    FIRFilter fir;
+
+    // Initialise the filter coefficient (the weight)
+    FIRFilter_init(&fir);
+
+    // Calculating the filtered values
+    for (int i = 0; i < length; i++) {
+        FIRFilter_calc(&fir, signal[i]);
+        filteredOutput[i] = fir.out;
+    }
+
+    // Plotting with GNU Plot
+    FILE *gnuplot = fopen("commands.p", "w");
+
+    if (filesystem::exists("commands.p")) {
+        char title[200] = "";
+        char xLabel[200] = "";
+        char yLabel[200] = "";
+
+        // Set the plot title, xlabel and ylabel
+        sprintf(title, "Original and filtered samples");
+        sprintf(xLabel, "Time");
+        sprintf(yLabel, "Amplitude");
+
+        fprintf(gnuplot, "set terminal wxt size 500, 400\n");
+        fprintf(gnuplot, "set title '%s'\n", title);
+        fprintf(gnuplot, "set xlabel '%s'\n", xLabel);
+        fprintf(gnuplot, "set ylabel '%s'\n", yLabel);
+
+        // Plot the data
+        fprintf(gnuplot, "plot '-' w lines lc rgb 'blue' title \"sampled data\", '-' w lines lc rgb 'red' title \"Filtered data\"\n");
+        for (int i = 0; i < length; i++) {
+            fprintf(gnuplot, "%d %f\n", i, signal[i]);
+        }
+
+        fprintf(gnuplot, "e\n");
+
+        for (int i = 0; i < length; i++) {
+            fprintf(gnuplot, "%d %f\n", i, filteredOutput[i]);
+        }
+
+        fprintf(gnuplot, "e\n");
+        fprintf(gnuplot, "replot\n");
+
+        // Close the GNU plot piping
+        fclose(gnuplot);
+
+        return true;
+    } else {
+        return false;
+    }
+}
