d8c9e46915ff90b5eb274bfea95ab1e2afa4c469
Desh Iyer <desh.iyer@gmail.com>
2023-02-25 15:05:12 +0530

    Update header file
    Fix redundant include statements by including them here only once in the entire project

--- include/FIR.h
+++ include/FIR.h
@@ -2,14 +2,17 @@
 #define FIR_FILTER_H
 
 // This is a bit overkill but saves writing out all the headers either in this file or another header to import here
-#include <bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
 
 // #define FIR_FILTER_LENGTH 5
 #define FIR_FILTER_LENGTH 10
 
 // Defining the struct to represent the circular buffer
-typedef struct
-{
+typedef struct {
     float buff[FIR_FILTER_LENGTH]; // For circular buffer array
     uint8_t buffIndex;             // Tracking the index of the circular buffer
     float out;                     // The output value of the circular buffer
