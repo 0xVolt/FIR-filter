6d42bd95f67052ecad3be091aa9f43aca34b6dde
Desh Iyer <desh.iyer@gmail.com>
2023-03-22 12:07:04 +0530

    Update script to generate signal

--- data/generated/generate-signal.py
+++ data/generated/generate-signal.py
@@ -5,36 +5,34 @@ import struct
 def wavToFloat(wave_file):
     w = wave.open(wave_file)
     astr = w.readframes(w.getnframes())
-    
-    # convert binary chunks to short 
+
+    # convert binary chunks to short
     a = struct.unpack("%ih" % (w.getnframes() * w.getnchannels()), astr)
-    
+
     a = [float(val) / np.log(2) for val in a]
     # a = [float(val) for val in a]
-    
+
     return a
 
 
 def writeSignal(signal, signalLength):
     outputFile = open('./data/generated/signal.txt', 'w')
-    
-    # outputFile.write(f'{signalLength}\n')
-    
+
     for dataPoint in signal:
         outputFile.write(f'{dataPoint}\n')
-        
+
     outputFile.close()
 
 
 def main():
     signal = wavToFloat(r'/home/volt/code/filter-coffee/data/raw/wrong-place-sound.wav')
-    
+
     signalLength = len(signal)
-    
+
     print("# Frames Read:", len(signal))
     print(f"Range of Values: [{str(min(signal))}, {str(max(signal))}]")
     print(type(signal))
-    
+
     writeSignal(signal, signalLength)
 
     print("Generated signal written to output file!")
